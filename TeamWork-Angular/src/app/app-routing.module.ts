0f5b4f0503eae165d96be8fd7743f2c6ff87e826
marinescuana11 <marinescuana28@gmail.com>
2021-03-27 07:28:08 +0200

    update ng update @angular/core @angular/cli --force to update Angular to version 11+

--- TeamWork-Angular/src/app/app-routing.module.ts
+++ TeamWork-Angular/src/app/app-routing.module.ts
@@ -68,7 +68,7 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule implements OnInit {

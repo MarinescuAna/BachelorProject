90120ab519fcecb419e6ab5c7948142dac008d37
marinescuana11 <marinescuana28@gmail.com>
2021-03-27 22:18:44 +0200

    create a temporary contact page

--- TeamWork-Angular/src/app/components/landing-pages-components/contact-page/contact-page.component.ts
+++ TeamWork-Angular/src/app/components/landing-pages-components/contact-page/contact-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/services/alert.service';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private injector: Injector,private route: Router,public authService: AuthService) { }
 
   ngOnInit(): void {
   }
+  onSubmit():void{
 
+    this.injector.get(AlertService).showSucces('The message was sent!');
+    if (this.authService.isLogged()) {
+      this.route.navigateByUrl('home-logged')
+    }
+    else {
+      this.route.navigateByUrl('landing-page')
+    }
+  }
 }

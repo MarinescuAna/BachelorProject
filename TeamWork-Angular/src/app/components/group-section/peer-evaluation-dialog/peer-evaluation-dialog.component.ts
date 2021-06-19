7df317274801098d531129a924b216f60caaaa45
marinescuana11 <marinescuana28@gmail.com>
2021-06-19 06:46:43 +0300

    Don't let the user to add grades grater than 100 or less than 0

--- TeamWork-Angular/src/app/components/group-section/peer-evaluation-dialog/peer-evaluation-dialog.component.ts
+++ TeamWork-Angular/src/app/components/group-section/peer-evaluation-dialog/peer-evaluation-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PeerEvaluationResultModule } from 'src/app/modules/peer-evaluation-result.module';
-import {UpdatePeerEvaluationModule} from 'src/app/modules/update-peer-evaluation.module';
+import { UpdatePeerEvaluationModule } from 'src/app/modules/update-peer-evaluation.module';
 import { PeerEvaluationService } from 'src/app/services/peer-evaluation.service';
 
 @Component({
@@ -12,26 +12,31 @@ import { PeerEvaluationService } from 'src/app/services/peer-evaluation.service'
 })
 export class PeerEvaluationDialogComponent implements OnInit {
   form = new FormGroup({
-    grade: new FormControl('',[Validators.required]),
-    comment: new FormControl('',[Validators.required]),
+    grade: new FormControl('', [Validators.required]),
+    comment: new FormControl('', [Validators.required]),
   });
   result: PeerEvaluationResultModule;
   constructor(
     public dialogRef: MatDialogRef<PeerEvaluationDialogComponent>,
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any,
-    private peerService:PeerEvaluationService
-  ) { 
-    this.result= data.data as PeerEvaluationResultModule;
+    private peerService: PeerEvaluationService
+  ) {
+    this.result = data.data as PeerEvaluationResultModule;
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(){
+  onSubmit() {
     let data = new UpdatePeerEvaluationModule();
-    data.peerEvaluationId=this.result.id;
-    data.grade=this.form.value.grade;
-    data.comments=this.form.value.comment;
+    data.peerEvaluationId = this.result.id;
+    data.grade = this.form.value.grade;
+    data.comments = this.form.value.comment;
+
+    if (parseInt(this.form.value.grade) < 0 || parseInt(this.form.value.grade) > 100) {
+      this.peerService.alertService.showWarning("The grade must be between 0 and 100!");
+      return;
+    }
     this.peerService.AssignPeerEvaluation(data).subscribe(cr => {
       this.peerService.alertService.showSucces("The grade was returned!");
       this.dialogRef.close();
